bfc3b1b288b77ac8b92240491350fefc14b7aef9
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-08-19 03:42:07 +0300

    Create App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/views/home_view.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -9,6 +10,9 @@ class NewsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
   }
 }
