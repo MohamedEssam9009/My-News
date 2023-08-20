1048a4bb43181e214aeb509e2bbe86a05d8fef22
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-08-21 01:22:19 +0300

    Trigger getnews method

--- lib/widgets/news_list_view.dart
+++ lib/widgets/news_list_view.dart
@@ -1,21 +1,39 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/services/news_services.dart';
 
 import 'news_tile.dart';
 
-class NewsListView extends StatelessWidget {
+class NewsListView extends StatefulWidget {
   const NewsListView({
     super.key,
   });
 
   @override
+  State<NewsListView> createState() => _NewsListViewState();
+}
+
+class _NewsListViewState extends State<NewsListView> {
+  List<ArticalModel> articles = [];
+
+  @override
+  void initState() async {
+    super.initState();
+    articles = await NewsServices(Dio()).getNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
-        childCount: 10,
+        childCount: articles.length,
         (context, index) {
-          return const Padding(
-            padding: EdgeInsets.only(bottom: 22.0),
-            child: NewsTile(),
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 22.0),
+            child: NewsTile(
+              articalModel: articles[index],
+            ),
           );
         },
       ),
--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
 
 class NewsTile extends StatelessWidget {
-  const NewsTile({super.key});
+  const NewsTile({super.key, required this.articalModel});
+
+  final ArticalModel articalModel;
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +13,23 @@ class NewsTile extends StatelessWidget {
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(6.0),
-          child: Image.network(
-            'https://scanbot.io/wp-content/uploads/2022/03/flutter_tutorial_hero.jpg',
-            height: 200.0,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
+          child: articalModel.image != null
+              ? Image.network(
+                  articalModel.image!,
+                  height: 200.0,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Placeholder(child: Image.asset('assets/placeholder.png')),
         ),
         const SizedBox(
           height: 12.0,
         ),
-        const Text(
-          'Large Title should be places in this place large ',
+        Text(
+          articalModel.title,
           maxLines: 2,
           overflow: TextOverflow.ellipsis,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.black87,
             fontSize: 20.0,
             fontWeight: FontWeight.w500,
@@ -33,13 +38,10 @@ class NewsTile extends StatelessWidget {
         const SizedBox(
           height: 8.0,
         ),
-        const Text(
-          'and here is the description of the news you place desc here',
+        Text(
+          articalModel.subTitle ?? '',
           maxLines: 2,
-          style: TextStyle(
-            color: Colors.grey,
-            fontSize: 14.0
-          ),
+          style: const TextStyle(color: Colors.grey, fontSize: 14.0),
         ),
       ],
     );
