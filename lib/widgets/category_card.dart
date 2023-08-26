d86b8168a099a91a3014696c0814482d9f9e4db2
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-08-27 00:16:44 +0300

    Create category view

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({super.key, required this.category});
@@ -8,25 +9,34 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 16.0),
-      child: Container(
-        height: 85.0,
-        width: 160.0,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(category.image),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const CategoryView(),
           ),
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        child: Center(
-          child: Text(
-            category.categoryName,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16.0),
+        child: Container(
+          height: 85.0,
+          width: 160.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(category.image),
+            ),
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Center(
+            child: Text(
+              category.categoryName,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
             ),
           ),
         ),
