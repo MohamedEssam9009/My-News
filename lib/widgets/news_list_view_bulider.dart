53e63d861f987aec950fd26ed280ec022458dafe
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-08-27 00:01:24 +0300

    Future builder part 2

--- lib/widgets/news_list_view_bulider.dart
+++ lib/widgets/news_list_view_bulider.dart
@@ -1,29 +1,50 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
 
 import '../services/news_services.dart';
+import 'error_message.dart';
 import 'news_list_view.dart';
 
-class NewsListViewBuilder extends StatelessWidget {
+class NewsListViewBuilder extends StatefulWidget {
   const NewsListViewBuilder({super.key});
 
   @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  var future;
+
+  @override
+  void initState() {
+    super.initState();
+    future = NewsServices(Dio()).getNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: NewsServices(Dio()).getNews(),
+    return FutureBuilder<List<ArticleModel>>(
+      future: future,
       builder: (context, snapshot) {
-        return NewsListView(
-          articles: snapshot.data ?? [],
-        );
+        if (snapshot.hasData) {
+          return NewsListView(
+            articles: snapshot.data!,
+          );
+        } else if (snapshot.hasError) {
+          return const SliverToBoxAdapter(
+            child: ErrorMassage(
+              message: 'Oops there was an error, try later ',
+            ),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
       },
     );
-    // return isLoading
-    //     ? const SliverToBoxAdapter(
-    //         child: Center(child: CircularProgressIndicator()))
-    //     : articles.isNotEmpty
-    //         ? NewsListView(articles: articles)
-    //         : const SliverToBoxAdapter(
-    //             child: Text('Oops there was an error, try later '),
-    //           );
   }
 }
