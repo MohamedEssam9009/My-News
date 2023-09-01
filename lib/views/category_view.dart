b71659ebe70c6875874d8b834e8a5476e8d1c8b5
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-01 03:28:48 +0300

    Debug code and fix bug

--- lib/views/category_view.dart
+++ lib/views/category_view.dart
@@ -9,12 +9,15 @@ class CategoryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          NewsListViewBuilder(
-            category: category,
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: CustomScrollView(
+          slivers: [
+            NewsListViewBuilder(
+              category: category,
+            )
+          ],
+        ),
       ),
     );
   }
